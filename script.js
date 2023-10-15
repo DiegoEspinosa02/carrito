4b08b282c78a502d0f03f40250bf91a13f7c446c
Diego Espinosa <21690075@tecvalles.mx>
2023-10-15 16:13:27 -0600

    x

---
+++ script.js
@@ -0,0 +1,27 @@
+function aumenta(){
+    document.getElementById('Price').style.fontSize='30px';
+    document.getElementById('Price').style.color='red';
+}
+function disminuye(){
+    document.getElementById('Price').style.fontSize='20px';
+    document.getElementById('Price').style.color='black';
+    
+}
+function ocultar(){
+    document.getElementById('infor').style.display='none';
+}
+function mostrar(){
+    document.getElementById('infor').style.display='block';
+}
+function atras(){
+    document.getElementById('cel').src='cel_atras.webp';
+}
+function frente(){
+    document.getElementById('cel').src='cel_frente.webp';
+}
+function izquierda(){
+    document.getElementById('cel').src='cel_izquierta.webp';
+}
+function derecha(){
+    document.getElementById('cel').src='cel_derecha.webp';
+}
